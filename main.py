11761e9a70f6553025f5163d1cc0e82dc1a44660
zsiegel92 <zsiegel92@gmail.com>
2026-04-02 17:18:47 -0400

    TODO

---
+++ main.py
@@ -0,0 +1,53 @@
+import modal
+import fastapi
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+image = modal.Image.debian_slim().uv_sync()
+
+app = modal.App('eightctl-web')
+
+
+
+def authorize_token(
+    token: HTTPAuthorizationCredentials = basic_http_bearer_dependency,
+) -> bool:
+    # TODO: rework this to get username/password from env vars and remove this comment
+    if settings.GROUPTHERE_SOLVER_API_KEY is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server API Error!",
+        )
+    if token.credentials != settings.GROUPTHERE_SOLVER_API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return True
+
+basic_http_bearer_dependency = Depends(HTTPBearer())
+
+webapp = FastAPI(
+    name="backend",
+    dependencies=[
+        Depends(authorize_token),
+    ],
+)
+
+@app.function(image=image)
+@modal.concurrent(max_inputs=100)
+@modal.asgi_app()
+def fastapi_app():
+    from fastapi import FastAPI, Request
+
+    web_app = FastAPI()
+
+
+    @web_app.post("/echo")
+    async def echo(request: Request):
+        body = await request.json()
+        return body
+
+    return web_app
